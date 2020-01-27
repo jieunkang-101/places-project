c6387143211edeec9ddc5e9183cbb087206e97da
Jieun Kang and jason stocks <jieun@>
2020-01-27 11:55:31 -0800

    add display html

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,38 @@ PlaceList.prototype.addPlace = function(place) {
   this.places.push(place);
 }
 // Business logic For Places project
-function Place(locale, landMark, timeOfYear, notes){
+function Place(placeName, locale, landmark, timeOfYear, notes){
+  this.placeName = placeName;
   this.locale = locale;
-  this.landMark = landMark;
+  this.landmark = landmark;
   this.timeOfYear = timeOfYear;
   this.notes = notes;
 }
 
+
+// User interface logic
 var placeList = new PlaceList();
-var Japan = Place("Tokyo", "Tokyo Tower", "dlfjdlfdj");
-console.log(placeList.addPlace(Japan));
+
+function displayPlaceList(placeListtoDisplay) {
+  var placeList = $("ul#places");
+  var htmlForPlacesInfo  = "";
+  placeListtoDisplay.places.forEach(function(place) {
+    htmlForPlaceInfo += "<li>" + place.locale + "</li>"
+  });
+  placeList.html(htmlForPlacesInfo);
+};
+
+$(document).ready(function() {
+  $("form#inputPlace").submit(function(event){
+    event.preventDefault();
+    var inputtedLocale = $("input#inputLocale").val();
+    var inputtedLandmark = $("input#inputLandmark").val();
+    var inputtedTimeOfYear = $("input#inputTimeOfYear").val();
+    var inputtedNotes = $("input#inputNotes").val();
+    var  newPlace = new Place (inputtedLocale, inputtedLandmark, inputtedTimeOfYear, inputtedNotes);
+    placeList.addPlace(newPlace);
+    //console.log(placeList.places);
+    displayPlaceList(placeList);
+  })
+});
+
